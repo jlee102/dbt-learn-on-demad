0dd09d30cb82bf6cb82b95521d1d9345a22e5006
jeffreylee102@gmail.com <jeffreylee102@gmail.com>
2021-04-22 03:08:12 +0000

    time revision

--- models/marts/core/crash/dim_times.sql
+++ models/marts/core/crash/dim_times.sql
@@ -1,6 +1,10 @@
-select
+with Crashtime as(
+    select
        {{ dbt_utils.surrogate_key('CRASH_DATE', 'CRASH_TIME') }} as surrogate_key,
         CRASH_DATE,
         Crash_TIME
 
-    FROM `etl-311323.Motor_Vehicle_Crash.collision` 
+    FROM `etl-311323.Motor_Vehicle_Crash.collision`
+)
+select * from Crashtime
+ 
