a9431ad7fa6d27e88622c6a4024d8b5a4d757dc6
jeffreylee102@gmail.com <jeffreylee102@gmail.com>
2021-04-23 01:30:05 +0000

    idk

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,30 +1,29 @@
-with orders as  (
-    select * from {{ ref('stg_orders' )}}
+with customers as (
+    select * from {{ ref('stg_customers')}}
 ),
-
-payments as (
-    select * from {{ ref('stg_payments') }}
+orders as (
+    select * from {{ ref('fct_orders')}}
 ),
-
-order_payments as (
+customer_orders as (
     select
-        order_id,
-        sum(case when status = 'success' then amount end) as amount
-
-    from payments
+        customer_id,
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
+    from orders
     group by 1
 ),
-
 final as (
-
     select
-        orders.order_id,
-        orders.customer_id,
-        orders.order_date,
-        coalesce(order_payments.amount, 0) as amount
-
-    from orders
-    left join order_payments using (order_id)
+        customers.customer_id,
+        customers.first_name,
+        customers.last_name,
+        customer_orders.first_order_date,
+        customer_orders.most_recent_order_date,
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_orders.lifetime_value
+    from customers
+    left join customer_orders using (customer_id)
 )
-
 select * from final
